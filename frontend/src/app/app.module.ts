6a24c8a0ee957fc7fee619d0ba2acbe079c43045
pramitha1166 <pramitha.wanigarathne@gmail.com>
2021-08-12 05:25:12 +0530

    setup hospital manager dashboard

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardHospitalmanagerComponent } from './components/hospitalmanager/dashboard-hospitalmanager/dashboard-hospitalmanager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardHospitalmanagerComponent
   ],
   imports: [
     BrowserModule,
