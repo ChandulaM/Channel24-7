785b0631b14a117c77a45e94f8bd6af061d609f4
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-08-21 21:28:35 +0530

    all crud completed

--- frontend/src/app/components/labassistant/manageresults/manageresults.component.ts
+++ frontend/src/app/components/labassistant/manageresults/manageresults.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-manageresults',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageresultsComponent implements OnInit {
 
-  constructor() { }
+  results: any = [];
+
+  constructor() {}
+
+  delete($event:any):void{
+    let id:Number = $event.target.parentNode.previousSibling.innerHTML;
+    let newResults:any= [];
+    this.results.forEach((data:any) => {
+      if(data.id != id){
+        newResults.push(data);
+      }
+    });
+    this.results = newResults;
+  }
 
   ngOnInit(): void {
+    axios
+      .get('http://localhost:8081/result/all')
+      .then((res) => {
+        this.results = res.data
+        console.log(res.data)
+      })
+      .catch((err) => console.log(err));
   }
 
 }
