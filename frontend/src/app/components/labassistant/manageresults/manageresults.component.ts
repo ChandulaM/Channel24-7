1eff88b89bc31bfb4f21cd31bbe475828b08a52b
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-09-29 00:30:47 +0530

    lab result serach/filter added

--- frontend/src/app/components/labassistant/manageresults/manageresults.component.ts
+++ frontend/src/app/components/labassistant/manageresults/manageresults.component.ts
@@ -4,33 +4,42 @@ import axios from 'axios';
 @Component({
   selector: 'app-manageresults',
   templateUrl: './manageresults.component.html',
-  styleUrls: ['./manageresults.component.css']
+  styleUrls: ['./manageresults.component.css'],
 })
 export class ManageresultsComponent implements OnInit {
-
   results: any = [];
 
   constructor() {}
 
-  delete($event:any):void{
-    let id:Number = $event.target.parentNode.previousSibling.innerHTML;
-    let newResults:any= [];
-    this.results.forEach((data:any) => {
-      if(data.id != id){
+  ngOnInit(): void {
+    axios
+      .get('http://localhost:8081/result/all')
+      .then((res) => {
+        this.results = res.data;
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  delete($event: any): void {
+    let id: Number = $event.target.parentNode.previousSibling.innerHTML;
+    let newResults: any = [];
+    this.results.forEach((data: any) => {
+      if (data.id != id) {
         newResults.push(data);
       }
     });
     this.results = newResults;
   }
 
-  ngOnInit(): void {
-    axios
-      .get('http://localhost:8081/result/all')
-      .then((res) => {
-        this.results = res.data
-        console.log(res.data)
-      })
-      .catch((err) => console.log(err));
+  search($event: any) {
+    let temp =$event.target.value;
+    let newResults: any = [];
+    this.results.forEach((data: any) => {
+      if (data.refNumber.includes(temp)) {
+        newResults.push(data);
+      }
+    });
+    this.results = newResults;
   }
-
 }
